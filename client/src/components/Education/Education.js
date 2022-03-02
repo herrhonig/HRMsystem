212972b266da5c901f3a361816ef88df115e3f7a
Danila Kilishov <k1lyaa100@gmail.com>
2022-03-02 15:17:47 +0300

    Candidate v 1.5

--- client/src/components/Education/Education.js
+++ client/src/components/Education/Education.js
@@ -3,28 +3,38 @@ import { Box, Container } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+import Divider from '@mui/material/Divider';
 
 const Education = ({place, startDate, endDate, direction, level}) => {
   return (
-    <Box sx={{display: 'flex', flexDirection: 'column', marginTop:'10px', borderRadius: 2, borderColor: 'grey.500'}}>
-      <Box sx={{display: 'flex', flexDirection: 'row'}}>
-        <Typography variant="button" display="block" gutterBottom>
+    <Stack
+      direction='column'
+      justifyContent='center'
+      alignItems='flex-start'
+      spacing={0.5}
+    >
+      <Stack
+        direction='row'
+        justifyContent='flex-start'
+        alignItems='center'
+        divider={<Divider orientation='vertical' flexItem />}
+        spacing={1}
+      >
+        <Typography variant="subtitle2" display="block" gutterBottom>
           {place}
         </Typography>
-        <AccountBalanceIcon sx={{marginLeft: '5px', marginRight: '5px'}} fontSize="small" />
-        <Typography variant="subtitle1" gutterBottom component="div">
+        <Typography variant="body2" gutterBottom component="div">
           {level}
         </Typography>
-        <Chip sx={{marginLeft: '5px', marginRight: '5px' }} label={startDate} variant="outlined" />
-        <Typography variant="subtitle1" gutterBottom component="div">
-          -
+        <Typography variant='body2' gutterBottom component='div'>
+        <Chip sx={{marginLeft: '5px', marginRight: '5px' }} label={`${startDate} - ${endDate}`} variant="outlined" />
         </Typography>
-        <Chip sx={{marginLeft: '5px', marginRight: '5px' }} label={endDate} variant="outlined" />
-      </Box>
-      <Typography variant="body2" gutterBottom>
+        </Stack>
+      <Typography variant="body2" gutterBottom mt='5px' mb='5px'>
         {direction}
       </Typography>
-    </Box>
+      </Stack>
   )
 }
 
