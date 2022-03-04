20a513fe1a70324d4afc37e8745d7e666f6d981b
Kirill Tcvetkov <ph.kirilltsvetkov@yandex.ru>
2022-03-04 14:30:00 +0300

    release final design

--- Новая папка с объектами/client/src/components/Loading/Loading.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import cls from './loader.module.css'
-
-export default function Loading() {
-  return (
-
-    <div className={cls.ldsdefault}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-  )
-}
--- Новая папка с объектами/server/routes/indexRouter.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const userRouter = require('./userRouter');
-
-router.use('/users', userRouter);
-
-module.exports = router;
